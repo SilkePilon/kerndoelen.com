f051d2bc49c6432dcde6812381228c9c18808ef9
Silke Pilon <silkepilon2009@gmail.com>
2022-06-15 08:22:28 +0000

    updted

--- github.py
+++ github.py
@@ -1,10 +1,13 @@
+import codecs
+
 import os
-with open("README.md", "r+") as F:
+with codecs.open("README.md", "r+", "utf-8") as F:
     readme = F.read()
     with open("test2.log", "r") as X:
         data = X.read()
     newdata = str(readme).replace("logdata", data)
     F.write(newdata)
+    F.write(u'\ufeff')
 os.system("git rm -r --cached --ignore-unmatch .")
 os.system("git add .")
 msg = input("message: ")
