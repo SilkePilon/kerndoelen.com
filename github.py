c4ddf5ebad33cced043de197667ade2edd5b7e23
Silke Pilon <silkepilon2009@gmail.com>
2022-06-15 13:10:52 +0000

    logs

--- github.py
+++ github.py
@@ -6,7 +6,7 @@ with open("server.log", "r") as x:
 with open("readme.txt", "r") as f:
     contents = f.readlines()
 
-log = str(log).replace("'", "").replace(",", "<br />").replace("\n", "").replace('"', '').replace("[", "").replace("]", "").replace("http://167.172.47.127:80", "i hide my ip")
+log = str(log).replace("'", "").replace(",", "\n").replace("\n", "").replace('"', '').replace("[", "").replace("]", "").replace("http://167.172.47.127:80", "i hide my ip")
 contents.insert(105, "```" + log + "```")
 
 
