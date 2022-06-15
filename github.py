8c364771a7d64dd3080a20d58a7ff252b7ce5840
Silke Pilon <silkepilon2009@gmail.com>
2022-06-15 12:26:07 +0000

    test

--- github.py
+++ github.py
@@ -6,7 +6,7 @@ with open("server.log", "r") as x:
 with open("readme.txt", "r") as f:
     contents = f.readlines()
 
-log = str(log).replace("'", "").replace(",", "")
+log = str(log).replace("'", "").replace(",", "<br />")
 contents.insert(105, log + "\n")
 
 
